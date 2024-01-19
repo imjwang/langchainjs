1f0098d8948634515fb7ec40bf6d9c2de7eec016
imjwang <ngjaywa@gmail.com>
2024-01-18 17:04:34 -0800

    ?

--- app/jokes/page.tsx
+++ app/jokes/page.tsx
@@ -1,53 +1,5 @@
-import { Client, type Dataset } from "langsmith";
 import { JokeDatasetManager } from "@/components/joke-dataset-manager";
-
-
-type DatasetProps = {
-  dataset: Dataset | null
-}
-
-async function Dataset({ dataset=null }: DatasetProps) {
-  if (!dataset) {
-    return null
-  }
-  return (
-    <div className="p-4 w-1/2">
-      <div className="bg-black p-2 text-white rounded-t-md">
-      <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
-      {dataset.name}      
-      </h3>
-      </div>
-      <div className="bg-slate-200 dark:bg-slate-600 py-6 rounded-b-md">
-      <ul className="ml-8 list-disc">
-        <li><b>Description:</b> {dataset.description}</li>
-        <li><b>Size:</b> {dataset.example_count}</li>
-      </ul>
-      </div>
-  </div>
-  )
-}
-
-async function getDatasets() {
-  const client = new Client()
-  const datasetsGenerator = client.listDatasets({datasetNameContains: "jokes"})
-  const datasets = []
-  
-  for await (const dataset of datasetsGenerator) {
-    datasets.push(dataset)
-  }
-
-  return datasets
-}
-
-async function DatasetVisualizer() {
-  // const datasets = await getDatasets()
-
-    return (
-      <div>
-        testing1234
-      </div>
-  )
-}
+import { DatasetVisualizer } from "@/components/dataset-viz";
 
 export default function JokesPage() {
 
---
+++ components/dataset-viz.tsx
@@ -0,0 +1,41 @@
+import { getJokeDatasetStatus } from "@/app/langsmith-actions";
+import { Client, type Dataset } from "langsmith";
+
+
+type DatasetProps = {
+  dataset: Dataset | null
+}
+
+async function Dataset({ dataset=null }: DatasetProps) {
+  if (!dataset) {
+    return null
+  }
+  return (
+    <div className="p-4 w-1/2">
+      <div className="bg-black p-2 text-white rounded-t-md">
+      <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
+      {dataset.name}      
+      </h3>
+      </div>
+      <div className="bg-slate-200 dark:bg-slate-600 py-6 rounded-b-md">
+      <ul className="ml-8 list-disc">
+        <li><b>Description:</b> {dataset.description}</li>
+        <li><b>Size:</b> {dataset.example_count}</li>
+      </ul>
+      </div>
+  </div>
+  )
+}
+
+
+export async function DatasetVisualizer() {
+  const dataset = await getJokeDatasetStatus()
+  
+  if (dataset == null) {
+    return null
+  }
+
+  return (
+    <Dataset dataset={dataset} key={dataset.id} />
+  )
+}
